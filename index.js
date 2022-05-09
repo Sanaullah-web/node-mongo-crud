961ab57c24e23e044696e8fa4324fb336e63eeb1
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-09 22:42:35 +0600

    some change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ConnectionPoolClosedEvent } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,15 +28,34 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        // const userCollection = client.db('foodExpress').collection('user');
+        const userCollection = client.db('foodExpress').collection('user');
         // const user = { name: 'ali', gmail: 'aksja@gmail.com' };
         // const result = await userCollection.insertOne(user);
         // console.log(`A document was inserted with the _id: ${result.insertedId}`);
 
-        app.post('/user', (req, res) => {
-            const user = req.body;
-            console.log(user)
-            res.send(user)
+        // showing data
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+
+        // posting data on mongoDB
+        app.post('/user', async (req, res) => {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            console.log(result)
+            res.send(result)
+        })
+
+        // Deleting 
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
         })
 
     }
